9a2fe36b65b858e579592d491bf5e77394fed371
Alexsey779 <fktrctq321@gmail.com>
2023-08-29 21:12:31 +0700

    first commit

---
+++ converter-color/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './App.css';
+import { ConverterColor } from './components/ConverterColor';
+
+const App: React.FC = () => {
+  return <ConverterColor />;
+}
+
+export default App;
